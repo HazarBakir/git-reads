8cc6760b541b24f9562e9d8dbf38860bb846b105
HazarBakir <hazar_bakir@protonmail.com>
2025-11-22 00:48:35 +0300

    feat: update index.html and App component for improved styling and animations
    - Changed favicon to a PNG logo for better branding.
    - Updated the title to "GitReads" for consistency.
    - Added a motion wrapper to the App component for smooth transitions.
    - Introduced new CSS variables for theming and layout adjustments.

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,24 @@ import { Routes, Route } from "react-router-dom";
 import Page from "./pages/Document";
 import { RepositoryProvider } from "@/contexts/RepositoryProvider";
 import Landing from "@/pages/Landing";
+import { motion } from "framer-motion";
 
 function App() {
   return (
-    <RepositoryProvider>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/document" element={<Page />} />
-        <Route path="/document/:sessionId" element={<Page />} />
-      </Routes>
-    </RepositoryProvider>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 1.2 }}
+    >
+      <RepositoryProvider>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/document" element={<Page />} />
+          <Route path="/document/:sessionId" element={<Page />} />
+        </Routes>
+      </RepositoryProvider>
+    </motion.div>
   );
 }
 
